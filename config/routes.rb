28470bed018036ca452ac1c0a36633f117eb4cdd
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-08-18 21:23:21 +0900

    picturesコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'sessions#new'
   resources :users, only: %i(new create show)
   resources :sessions, only: %i(new create destroy)
+  resources :pictures
 end
