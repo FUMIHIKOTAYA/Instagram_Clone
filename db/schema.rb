d81e8fbf20a08b3f582e3363fc2ddc3197bce419
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-08-22 07:50:01 +0900

    Usersテーブルにプロフィールカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_020402) do
+ActiveRecord::Schema.define(version: 2020_08_21_224230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_08_21_020402) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile"
+    t.string "user_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
