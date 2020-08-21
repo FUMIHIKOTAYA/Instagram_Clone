1a89f57b0ea07072dd3036d1ee3a9409951286af
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-08-21 16:34:37 +0900

    アクセス制限の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,7 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: %i(show edit update destroy)
+  before_action :limit_access, only: %i(edit update destroy)
+  before_action :authenticate_user, only: %i(index)
 
   def index
     @pictures = Picture.all.order(id: %q(DESC))
@@ -15,7 +17,7 @@ class PicturesController < ApplicationController
       render :new
     else
       if @picture.save
-        redirect_to pictures_path, notice: '投稿しました！'
+        redirect_to pictures_path, notice: %q(投稿しました。)
       else
         render :new
       end
@@ -23,15 +25,14 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @favorite = current_user.favorites.find_by(picture_id: @picutre.id)
-  end
+      @favorite = current_user.favorites.find_by(picture_id: @picutre.id)
+    end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @picture.update(picture_params)
-      redirect_to pictures_path, notice: '投稿を編集しました！'
+      redirect_to pictures_path, notice: %q(投稿を編集しました。)
     else
       render :edit
     end
@@ -44,7 +45,7 @@ class PicturesController < ApplicationController
 
   def destroy
     @picture.destroy
-    redirect_to picture_path, notice: '投稿を削除しました！'
+    redirect_to picture_path, notice: %q(投稿を削除しました。)
   end
 
   private
@@ -56,4 +57,18 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
+  def limit_access
+    @picture = Picture.find_by(id: params[:id])
+    unless @picture.user_id == current_user.id
+      redirect_to pictures_path, notice: %q(実行権限がありません。)
+    end
+  end
+
+  def authenticate_user
+    @current_user = User.find_by(id: session[:user_id])
+    if @current_user == nil
+      flash[:notice] = %q(ログインして始められます。)
+      redirect_to new_session_path
+    end
+  end
 end
